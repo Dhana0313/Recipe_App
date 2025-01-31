e3d7875d7c86a4e7823731973879a07282b99db9
Dhananjaya <dananjayajayaweera1262@gmail.com>
2025-01-31 22:09:20 +0530

    add some js part

--- script.js
+++ script.js
@@ -2,8 +2,29 @@ const searchBox = document.querySelector('.searchBox');
 const searchBtn = document.querySelector('.searchBtn');
 const recipeContainer = document.querySelector('.recipe-container');
 
+//Function to get recipes
+const fetchRecipes = async (query) => {
+  const data= await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+  const response = await data.json();
+
+  response.meals.forEach(meal => {
+    const recipeDiv = document.createElement('div');
+    recipeDiv.classList.add('recipe');
+    recipeDiv.innerHTML = `
+      <img src="${meal.strMealThumb}">
+
+    `
+    recipeContainer.appendChild(recipeDiv);
+
+  });
+  
+
+}
+
 searchBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  console.log("Button Clicked");
+  const searchInput = searchBox.value.trim();
+  fetchRecipes(searchInput);
+  //console.log("Button Clicked");
 });
 
