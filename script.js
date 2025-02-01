f30aae8e6781d70a26af7cf2570a391f7e225216
Dhananjaya <dananjayajayaweera1262@gmail.com>
2025-02-01 09:21:48 +0530

    Filled main section

--- script.js
+++ script.js
@@ -4,21 +4,25 @@ const recipeContainer = document.querySelector('.recipe-container');
 
 //Function to get recipes
 const fetchRecipes = async (query) => {
+  recipeContainer.innerHTML = "<h2>Fetching Recipes...</h2>";
   const data= await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
   const response = await data.json();
 
+  recipeContainer.innerHTML="";
   response.meals.forEach(meal => {
     const recipeDiv = document.createElement('div');
     recipeDiv.classList.add('recipe');
     recipeDiv.innerHTML = `
       <img src="${meal.strMealThumb}">
+      <h3>${meal.strMeal}</h3>
+      <p>${meal.strArea}</p>
+      <p>${meal.strCategory}</p>
 
     `
     recipeContainer.appendChild(recipeDiv);
 
   });
   
-
 }
 
 searchBtn.addEventListener('click', (e) => {
