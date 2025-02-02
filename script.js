150f1bcbaa3bbdc8f910b4ee0be431d116c436a6
Dhananjaya <dananjayajayaweera1262@gmail.com>
2025-02-02 13:18:28 +0530

    Add css styles to recipe content

--- script.js
+++ script.js
@@ -15,10 +15,19 @@ const fetchRecipes = async (query) => {
     recipeDiv.innerHTML = `
       <img src="${meal.strMealThumb}">
       <h3>${meal.strMeal}</h3>
-      <p>${meal.strArea}</p>
-      <p>${meal.strCategory}</p>
+      <p><span>${meal.strArea}</span> Dish</p>
+      <p>Belongs to <span>${meal.strCategory}</span> Category</p>
 
     `
+    const button = document.createElement('button');
+    button.textContent = "View Recipe";
+    recipeDiv.appendChild(button);
+
+    //Adding EventListener to recipe button
+    button.addEventListener('click',()=>{
+      openRecipePopup(meal);
+    })
+
     recipeContainer.appendChild(recipeDiv);
 
   });
