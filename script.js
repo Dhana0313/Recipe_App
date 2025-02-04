26363d43df1c421a0710d299435461f9d4fccd63
Dhananjaya <dananjayajayaweera1262@gmail.com>
2025-02-04 09:27:07 +0530

    Adding scrollbar and finalizing..

--- script.js
+++ script.js
@@ -7,33 +7,37 @@ const recipeCloseBtn = document.querySelector('.recipe-close-btn');
 //Function to get recipes
 const fetchRecipes = async (query) => {
   recipeContainer.innerHTML = "<h2>Fetching Recipes...</h2>";
-  const data= await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
-  const response = await data.json();
+  try {
+    const data= await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    const response = await data.json();
 
-  recipeContainer.innerHTML="";
-  response.meals.forEach(meal => {
-    const recipeDiv = document.createElement('div');
-    recipeDiv.classList.add('recipe');
-    recipeDiv.innerHTML = `
-      <img src="${meal.strMealThumb}">
-      <h3>${meal.strMeal}</h3>
-      <p><span>${meal.strArea}</span> Dish</p>
-      <p>Belongs to <span>${meal.strCategory}</span> Category</p>
+    recipeContainer.innerHTML="";
+    response.meals.forEach(meal => {
+      const recipeDiv = document.createElement('div');
+      recipeDiv.classList.add('recipe');
+      recipeDiv.innerHTML = `
+        <img src="${meal.strMealThumb}">
+        <h3>${meal.strMeal}</h3>
+        <p><span>${meal.strArea}</span> Dish</p>
+        <p>Belongs to <span>${meal.strCategory}</span> Category</p>
 
-    `
-    const button = document.createElement('button');
-    button.textContent = "View Recipe";
-    recipeDiv.appendChild(button);
+      `
+      const button = document.createElement('button');
+      button.textContent = "View Recipe";
+      recipeDiv.appendChild(button);
 
-    //Adding EventListener to recipe button
-    button.addEventListener('click',()=>{
-      openRecipePopup(meal);
-    })
+      //Adding EventListener to recipe button
+      button.addEventListener('click',()=>{
+        openRecipePopup(meal);
+      })
 
-    recipeContainer.appendChild(recipeDiv);
+      recipeContainer.appendChild(recipeDiv);
 
-  });
-  
+    });
+  }
+  catch(error){
+    recipeContainer.innerHTML = "<h2>Error in Fetching Recipes...</h2>"
+  }
 }
 
 //Function to fetch ingredients and measurements
@@ -76,6 +80,10 @@ recipeCloseBtn.addEventListener('click', () => {
 searchBtn.addEventListener('click', (e) => {
   e.preventDefault();
   const searchInput = searchBox.value.trim();
+  if(!searchInput){
+    recipeContainer.innerHTML = `<h2>Type the meal in the search box.</h2>`;
+    return;
+  }
   fetchRecipes(searchInput);
   //console.log("Button Clicked");
 });
